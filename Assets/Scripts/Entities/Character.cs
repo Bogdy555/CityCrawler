9c2324c7ff7de8c7467da1e51694b1e240da0987
Bogdy555 <florescubogdan555@gmail.com>
2025-01-14 08:09:00 +0200

    Little by little...

--- Assets/Scripts/Entities/Character.cs
+++ Assets/Scripts/Entities/Character.cs
@@ -1,82 +1,96 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Character : MonoBehaviour
 {
-    [SerializeField]
-    float Force = 10.0f;
+	[SerializeField]
+	float Force = 10.0f;
 
-    [SerializeField]
-    float FallForce = 3.0f;
+	[SerializeField]
+	float FallForce = 3.0f;
 
-    [SerializeField]
-    float TopSpeed = 5.0f;
+	[SerializeField]
+	float TopSpeed = 5.0f;
 
-    [SerializeField]
-    float TopFallSpeed = 10.0f;
+	[SerializeField]
+	float TopFallSpeed = 10.0f;
 
-    [SerializeField]
-    float JumpSpeed = 5.0f;
+	[SerializeField]
+	float JumpSpeed = 5.0f;
 
-    [SerializeField]
-    LayerMask TerrainLayer;
+	[SerializeField]
+	LayerMask TerrainLayer;
 
-    [SerializeField]
-    float castDistanceDown = 0.5f;
+	[SerializeField]
+	float castDistanceDown = 0.5f;
 
-    [SerializeField]
-    Vector2 groundDetectionBoxSize = new Vector2(0.8f, 0.3f);
+	[SerializeField]
+	Vector2 groundDetectionBoxSize = new Vector2(0.8f, 0.3f);
 
-    public bool grounded = false;
+	public bool grounded = false;
 
-    Rigidbody2D RigidBody;
+	Rigidbody2D RigidBody;
 
-    void Start()
-    {
-        RigidBody = GetComponent<Rigidbody2D>();
-    }
+	void Start()
+	{
+		RigidBody = GetComponent<Rigidbody2D>();
+	}
 
-    void Update()
-    {
-        grounded = IsGrounded();
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			SceneManager.LoadScene("LevelSelect");
+		}
 
-        if(Input.GetKey(KeyCode.A) && RigidBody.velocity.x > -TopSpeed)
-        {
-            RigidBody.AddForce(new Vector2(-1.0f, 0.0f) * Force);
-        }
+		grounded = IsGrounded();
 
-        if (Input.GetKey(KeyCode.D) && RigidBody.velocity.x < TopSpeed)
-        {
-            RigidBody.AddForce(new Vector2(1.0f, 0.0f) * Force);
-        }
+		if(Input.GetKey(KeyCode.A) && RigidBody.velocity.x > -TopSpeed)
+		{
+			RigidBody.AddForce(new Vector2(-1.0f, 0.0f) * Force);
+		}
 
-        if (Input.GetKey(KeyCode.S) && RigidBody.velocity.y > -TopFallSpeed)
-        {
-            RigidBody.AddForce(new Vector2(0.0f, -1.0f) * FallForce);
-        }
+		if (Input.GetKey(KeyCode.D) && RigidBody.velocity.x < TopSpeed)
+		{
+			RigidBody.AddForce(new Vector2(1.0f, 0.0f) * Force);
+		}
 
-        if(Input.GetKey(KeyCode.Space) && grounded)
-        {
-            RigidBody.velocity = new Vector2(RigidBody.velocity.x, JumpSpeed);
-        }
-    }
+		if (Input.GetKey(KeyCode.S) && RigidBody.velocity.y > -TopFallSpeed)
+		{
+			RigidBody.AddForce(new Vector2(0.0f, -1.0f) * FallForce);
+		}
 
-    bool IsGrounded()
-    {
-        if (Physics2D.BoxCast(transform.position, groundDetectionBoxSize, 0, -transform.up, castDistanceDown, TerrainLayer))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+		if(Input.GetKey(KeyCode.Space) && grounded)
+		{
+			RigidBody.velocity = new Vector2(RigidBody.velocity.x, JumpSpeed);
+		}
+	}
 
-        void OnDrawGizmos()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireCube(transform.position - transform.up * castDistanceDown, groundDetectionBoxSize);
-    }
+	bool IsGrounded()
+	{
+		if (Physics2D.BoxCast(transform.position, groundDetectionBoxSize, 0, -transform.up, castDistanceDown, TerrainLayer))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Mobs")
+		{
+			SceneManager.LoadScene("LevelSelect");
+		}
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(transform.position - transform.up * castDistanceDown, groundDetectionBoxSize);
+	}
 }
